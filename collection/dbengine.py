bbe2dad22a5543937cab22e495429c250e75a4f7
p-victor <irap4fun@gmail.com>
2014-07-05 04:10:41 +0200

    added columns to sql alchemy class

--- collection/dbengine.py
+++ collection/dbengine.py
@@ -8,3 +8,9 @@ db = create_engine('sqlite:///%s' %path)
 metadata = BoundMetaData(db)
 
 
+metrics = Table('os_metrics', metadata\
+                Column('id', Integer, primary_key=True)
+                Column('cpu_load', float),
+                Column('cpu_percent' float),
+                Column('swap_memory', float)
+            )
